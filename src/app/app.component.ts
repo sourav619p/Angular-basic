3c234c767b9b88a49021574c2bdbe52c4ccc65ae
Sourav Prakash Mj <souravprakash619@gmail.com>
2024-02-05 17:23:04 +0530

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,112 @@
 import { Component } from '@angular/core';
+import { json } from 'stream/consumers';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'test';
-}
+  public  title = 'World';
+  // Template Driven Form
+  // data = {
+  //   firstname:'',
+  //   lastname:'',
+  //   Phonenumber:'',
+  //   email:''
+  // }
+  // onsubmit(){
+  //   alert(JSON.stringify(this.data))
+  // }
+
+  // Reactive Forms
+  // constructor(private fb:FormBuilder){}
+  // submit = false;
+  // registrationForm = this.fb.group({
+  //   firstName:['',Validators.required],
+  //   lastName:[''],
+  //   phone:[''],
+  //   email:['']
+  // })
+
+  // get f() {
+  //   return this.registrationForm.controls;
+  // }
+
+  // onsubmit(){
+  //   this.submit = true;
+  //   console.log('clicked');
+  //   console.log('f',this.f)
+  // }
+
+  //Reactive Dynamic Form
+  get alternativeEmails(){
+    return this.registration.get('alternativeEmails') as FormArray
+  }
+
+  addAlternativeEmail(){
+    console.log('alternativeEmails',this.alternativeEmails)
+    this.alternativeEmails.push(this.fb.control(''));
+  }
+
+  constructor(private fb: FormBuilder){}
+
+  registration = this.fb.group({
+    email:['IT Master'],
+    alternativeEmails: this.fb.array([])
+  })
+
+  onSubmit(values : FormGroup){
+   
+  }
+
+//   public numSystem = 'Even'
+//   public count = 0;
+//   public status = 'Even'
+//   public isHidden = true;
+//   data = {
+//    one: 'one',
+//    two: 'two'
+//   }
+
+//   public fruits =[
+//     {name:"lal",job:"software"},
+//     {name:"amal",job:"security"},
+//     {name:"ram",job:"network"}
+//   ]
+
+//   colorFunction(){
+//    return 'red'
+//   }
+ 
+//  UpdateCount(operation : string){
+//    if(operation === 'Increment'){
+//      this.count += 1;
+//    }else{
+//      if (this.count != 0){
+//      this.count -= 1;
+//      }
+//    }
+//    if(this.count % 2 == 0){
+//      this.numSystem = 'Even'
+//    }else{
+//      this.numSystem = 'Odd'
+//    }
+//  }
+//  checkNumber(){
+//   this.isHidden = false;
+//   if(this.count % 2 == 0){
+//     this.status = 'Even'
+//   }else{
+//     this.status = 'Odd'
+//   }
+//  }
+//  clear(){
+//   this.isHidden = true;
+//   this.count = 0;
+//  }
+
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
